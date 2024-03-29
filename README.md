3ea3db4f200533189a9d5439fd53656038f28524
Aapolo99 <giraldoblandonadriana707@gmail.com>
2024-03-28 20:34:11 -0500

    Grafico completado

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # IA-Busqueda-Y-Sistemas-Basados-En-Reglas
-Búsqueda Y Sistemas Basados En Reglas.
+Busqueda Y Sistemas Basados En Reglas.
+
+Hay que hacer, en Python, importación de las librerías:  
+
+Pandas: Para operar y analizar la data. 
+
+Numpy: Para manejar los arreglos. 
+
+Networkx: Para ayudar a visualizar el grafico.  
+
+Matplotlib: Para la interactividad de la visualización estática para el análisis de la data sobre las rutas de TransMilenio. 
+
+Graficar usando networkx introduciendo origen y destino de las rutas. 
+
+Aplicar algoritmo Dijkstra determinando la mejor ruta entre los puntos A y B. Dijkstra posee interactividad, proporcionando rutas cortas partiendo de un nodo inicial particular a los demás nodos en el gráfico. 
--- script.py
+++ script.py
@@ -1,22 +1,27 @@
+import math
 import pandas as pd
 import numpy as np
 import networkx as nx
 from collections import Counter
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 from tabulate import tabulate
 
 dataset = pd.read_csv('./dataset.csv', index_col=None) # Importamos el dataset
 
-# Se crea un grafico con los datos del dataset
+# Se crea un grafo con los datos del dataset
 TRANSMI = nx.from_pandas_edgelist(dataset, source='origen', target='destino', edge_attr='longitud de interaccion')
+# TRANSMI.nodes() Nodos
+# TRANSMI.edges() Aristas
+# TRANSMI.order() Ordenar elementos
 
 # Se utiliza el algoritmo de Dijkstra para saber la ruta más rápida entre destinos A y B
 destinos = [
-    ['Portal Tunal', 'Portal Suba'],
-    ['Olaya', 'Portal Suba'],
-    ['Gratamira', 'Flores'],
-    ['General Santander', 'Portal Tunal'],
-    ['Portal Norte', 'Portal 80']
+    ['Portal Norte', 'Portal 80'],
+    ['Ricaurte', 'General Santander'],
+    ['Perdomo', 'Flores'],
+    ['Tercer Milenio', 'Portal Tunal'],
+    ['Calle 100', 'Madelena']
 ]
 
 posibles_rutas = []
@@ -27,24 +32,39 @@ for destino in destinos:
 # Result
 print(tabulate(posibles_rutas, tablefmt="pretty"))
 
-# Se Traen algunas estaciones destino del dataset
+# Traemos algunas estaciones destino del dataset
 algunas_estaciones = dataset[['destino']]
-# Se convierte a un Array
+# Lo convertidos a un Array
 destinos = algunas_estaciones.to_numpy()
-# Se define un array vacio para guardar el numero de rutas por estacion
+# Definimos un array vacio para guardar el numero de rutas por estacion
 rutas = []
-# Se eliminan los valores repetidos del array
+# Eliminamos los valores repetidos del array
 portales = np.unique(destinos)
 
-# se Itera sobre el array para saber la cantidad de rutas
+# Iteramos sobre el array para saber la cantidad de rutas
 for portal in portales:
-# se cuentan las coincidencias de la estación en las rutas destino
+  # Contamos las coincidencias de la estación en las rutas destino
   cantidad_rutas = np.count_nonzero(destinos == portal)
-  # se guarda el valor
+  # Guardamos el valor
   rutas.append(cantidad_rutas)
+  
+#plt.ion()
+plt.plot(portales,rutas,lw=5, c='y', marker='o', ms=10, mfc='b')
 
-# Se cambia el tamaño de la gráfica
-plt.figure(figsize=(15, 5))
-# se Define los valores de la gráfica de dispersión
-plt.scatter(portales, rutas)
+
+# Cambiamos el tamaño de la gráfica
+#plt.figure(figsize=(15, 7))
+# Definimos los valores de la gráfica de dispersión
+#plt.scatter(portales, rutas)
+
+
+#Colocamos las etiquetas de los ejes
+plt.xlabel("Estaciones")
+plt.ylabel("Cantidad De Rutas")
+
+#Colocamos la leyenda
+#plt.legend(['Rutas','Estaciones'])
+
+#Colocamos el título del gráfico
+plt.title("Gráfica De Dispersión")
 plt.show()
